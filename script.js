02a9500a7761c9b407cafb322c1b8cd2f8f281e8
samra <76221460+SamraZoma@users.noreply.github.com>
2021-08-27 02:47:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.containerbody')
 
 
 
+open.addEventListener('click',() => container.classList.add('show-nav'))
+close.addEventListener('click',() => container.classList.remove('show-nav'))
 
+/*  contact  */
 
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
